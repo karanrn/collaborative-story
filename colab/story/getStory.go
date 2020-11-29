5626aa62e0c92eb96c96950577a7e57001633f61
Karan Nadagoudar <karan4080@gmail.com>
2020-11-29 10:29:29 +0530

    Corrected logic for fetching unfinished story

--- colab/story/getStory.go
+++ colab/story/getStory.go
@@ -18,14 +18,14 @@ type storySentence struct {
 */
 
 type storyParagraph struct {
-	ID            string   `json:"-"`
-	StartSentence string   `json:"-"`
-	EndSentence   string   `json:"-"`
+	ID            int      `json:"-"`
+	StartSentence int      `json:"-"`
+	EndSentence   int      `json:"-"`
 	Sentences     []string `json:"sentences"`
 }
 
 type detailedStory struct {
-	ID             string           `json:"id"`
+	ID             int              `json:"id"`
 	Title          string           `json:"title"`
 	StartParagraph int              `json:"-"`
 	EndParagraph   int              `json:"-"`
@@ -64,27 +64,16 @@ func GetStory(w http.ResponseWriter, r *http.Request) {
 	}
 
 	// Get paragraph details from paragraph table
-	paragraphStmt, err := db.Query()
-	if err != nil {
-		log.Println(err.Error())
-		w.WriteHeader(http.StatusInternalServerError)
-		json.NewEncoder(w).Encode(`{'error': 'internal server error'}`)
-		return
-	}
-	defer paragraphStmt.Close()
-
-	var tmpParagraph storyParagraph
-	for paragraphStmt.Next() {
-		err = paragraphStmt.Scan(&tmpParagraph.ID, &tmpParagraph.StartSentence, &tmpParagraph.EndSentence)
-		if err != nil {
-			log.Println(err.Error())
-			w.WriteHeader(http.StatusInternalServerError)
-			json.NewEncoder(w).Encode(`{'error': 'internal server error'}`)
-			return
+	if resStory.StartParagraph > 0 {
+		var paraQuery string
+		// Consideration for unfinished story
+		if resStory.EndParagraph == 0 {
+			paraQuery = fmt.Sprintf("Select paragraph_id, start_sentence, ifnull(end_sentence, 0) from paragraph where paragraph_id >= %d", resStory.StartParagraph)
+		} else {
+			paraQuery = fmt.Sprintf("Select paragraph_id, start_sentence, end_sentence from paragraph where paragraph_id >= %d and paragraph_id < %d", resStory.StartParagraph, resStory.EndParagraph)
 		}
 
-		// Get sentences of the story
-		sentenceStmt, err := db.Query(fmt.Sprintf("Select word from sentence where sentence_id >= %d and sentence_id < %d", resStory.StartParagraph, resStory.EndParagraph))
+		paragraphStmt, err := db.Query(paraQuery)
 		if err != nil {
 			log.Println(err.Error())
 			w.WriteHeader(http.StatusInternalServerError)
@@ -93,9 +82,10 @@ func GetStory(w http.ResponseWriter, r *http.Request) {
 		}
 		defer paragraphStmt.Close()
 
-		var word string
-		for sentenceStmt.Next() {
-			err = sentenceStmt.Scan(&word)
+		var tmpParagraph storyParagraph
+		var sentenceQuery string
+		for paragraphStmt.Next() {
+			err = paragraphStmt.Scan(&tmpParagraph.ID, &tmpParagraph.StartSentence, &tmpParagraph.EndSentence)
 			if err != nil {
 				log.Println(err.Error())
 				w.WriteHeader(http.StatusInternalServerError)
@@ -103,10 +93,37 @@ func GetStory(w http.ResponseWriter, r *http.Request) {
 				return
 			}
 
-			tmpParagraph.Sentences = append(tmpParagraph.Sentences, word)
-		}
+			// Consideration for unfinished paragraph
+			if tmpParagraph.EndSentence == 0 {
+				sentenceQuery = fmt.Sprintf("Select word from sentence where sentence_id >= %d", tmpParagraph.StartSentence)
+			} else {
+				sentenceQuery = fmt.Sprintf("Select word from sentence where sentence_id >= %d and sentence_id < %d", tmpParagraph.StartSentence, tmpParagraph.EndSentence)
+			}
+			// Get sentences of the story
+			sentenceStmt, err := db.Query(sentenceQuery)
+			if err != nil {
+				log.Println(err.Error())
+				w.WriteHeader(http.StatusInternalServerError)
+				json.NewEncoder(w).Encode(`{'error': 'internal server error'}`)
+				return
+			}
+			defer paragraphStmt.Close()
 
-		resStory.Paragraphs = append(resStory.Paragraphs, tmpParagraph)
+			var word string
+			for sentenceStmt.Next() {
+				err = sentenceStmt.Scan(&word)
+				if err != nil {
+					log.Println(err.Error())
+					w.WriteHeader(http.StatusInternalServerError)
+					json.NewEncoder(w).Encode(`{'error': 'internal server error'}`)
+					return
+				}
+
+				tmpParagraph.Sentences = append(tmpParagraph.Sentences, word)
+			}
+
+			resStory.Paragraphs = append(resStory.Paragraphs, tmpParagraph)
+		}
 	}
 
 	result, err := json.Marshal(&resStory)
