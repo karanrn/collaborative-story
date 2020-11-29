b7aa2a3a07ffc3a427d3534a68f0006482f4d98b
Karan Nadagoudar <karan4080@gmail.com>
2020-11-29 10:18:53 +0530

    Added GetStory method to fetch specific story

--- colab/story/getStory.go
+++ colab/story/getStory.go
@@ -1,27 +1,120 @@
 package story
 
 import (
+	"encoding/json"
 	"fmt"
+	"log"
 	"net/http"
 
 	"github.com/gorilla/mux"
+
+	"CollaborativeStory/database"
 )
 
+/*
+type storySentence struct {
+	words []string
+}
+*/
+
 type storyParagraph struct {
-	Sentences []string `json:"sentences"`
+	ID            string   `json:"-"`
+	StartSentence string   `json:"-"`
+	EndSentence   string   `json:"-"`
+	Sentences     []string `json:"sentences"`
 }
 
-type detStory struct {
-	ID         string           `json:"id"`
-	Title      string           `json:"title"`
-	CreatedAt  string           `json:"created_at"`
-	UpdatedAt  string           `json:"updated_at"`
-	Paragraphs []storyParagraph `json:"paragraphs"`
+type detailedStory struct {
+	ID             string           `json:"id"`
+	Title          string           `json:"title"`
+	StartParagraph int              `json:"-"`
+	EndParagraph   int              `json:"-"`
+	CreatedAt      string           `json:"created_at"`
+	UpdatedAt      string           `json:"updated_at"`
+	Paragraphs     []storyParagraph `json:"paragraphs"`
 }
 
 // GetStory gets the specific story basis story_id
 func GetStory(w http.ResponseWriter, r *http.Request) {
 	storyID := mux.Vars(r)["id"]
 
-	fmt.Println(storyID)
+	var resStory detailedStory
+
+	db := database.DBConn()
+	defer db.Close()
+
+	// Get story details from story table
+	storyStmt, err := db.Query(fmt.Sprintf("Select story_id, title, ifnull(start_paragraph, 0), ifnull(end_paragraph, 0), created_at, updated_at from story where story_id = %s", storyID))
+	if err != nil {
+		log.Println(err.Error())
+		w.WriteHeader(http.StatusInternalServerError)
+		json.NewEncoder(w).Encode(`{'error': 'internal server error'}`)
+		return
+	}
+	defer storyStmt.Close()
+
+	if storyStmt.Next() {
+		err = storyStmt.Scan(&resStory.ID, &resStory.Title, &resStory.StartParagraph, &resStory.EndParagraph, &resStory.CreatedAt, &resStory.UpdatedAt)
+		if err != nil {
+			log.Println(err.Error())
+			w.WriteHeader(http.StatusInternalServerError)
+			json.NewEncoder(w).Encode(`{'error': 'internal server error'}`)
+			return
+		}
+	}
+
+	// Get paragraph details from paragraph table
+	paragraphStmt, err := db.Query()
+	if err != nil {
+		log.Println(err.Error())
+		w.WriteHeader(http.StatusInternalServerError)
+		json.NewEncoder(w).Encode(`{'error': 'internal server error'}`)
+		return
+	}
+	defer paragraphStmt.Close()
+
+	var tmpParagraph storyParagraph
+	for paragraphStmt.Next() {
+		err = paragraphStmt.Scan(&tmpParagraph.ID, &tmpParagraph.StartSentence, &tmpParagraph.EndSentence)
+		if err != nil {
+			log.Println(err.Error())
+			w.WriteHeader(http.StatusInternalServerError)
+			json.NewEncoder(w).Encode(`{'error': 'internal server error'}`)
+			return
+		}
+
+		// Get sentences of the story
+		sentenceStmt, err := db.Query(fmt.Sprintf("Select word from sentence where sentence_id >= %d and sentence_id < %d", resStory.StartParagraph, resStory.EndParagraph))
+		if err != nil {
+			log.Println(err.Error())
+			w.WriteHeader(http.StatusInternalServerError)
+			json.NewEncoder(w).Encode(`{'error': 'internal server error'}`)
+			return
+		}
+		defer paragraphStmt.Close()
+
+		var word string
+		for sentenceStmt.Next() {
+			err = sentenceStmt.Scan(&word)
+			if err != nil {
+				log.Println(err.Error())
+				w.WriteHeader(http.StatusInternalServerError)
+				json.NewEncoder(w).Encode(`{'error': 'internal server error'}`)
+				return
+			}
+
+			tmpParagraph.Sentences = append(tmpParagraph.Sentences, word)
+		}
+
+		resStory.Paragraphs = append(resStory.Paragraphs, tmpParagraph)
+	}
+
+	result, err := json.Marshal(&resStory)
+	if err != nil {
+		w.WriteHeader(http.StatusInternalServerError)
+		json.NewEncoder(w).Encode(`{'error': 'internal server error'}`)
+		return
+	}
+	w.WriteHeader(http.StatusOK)
+	json.NewEncoder(w).Encode(string(result))
 }
--- main.go
+++ main.go
@@ -15,9 +15,9 @@ func main() {
 	// HTTP multiplexer/router
 	router := mux.NewRouter().StrictSlash(false)
 
-	router.HandleFunc("/add", story.AddToStory)               // POST method to add word to story
-	router.HandleFunc("/stories", story.GetStories)           // GET method to list/get all stories
-	router.HandleFunc("/stories/{id:[0-9]+}", story.GetStory) // GET to get specific story
+	router.HandleFunc("/add", story.AddToStory).Methods("POST")              // POST method to add word to story
+	router.HandleFunc("/stories", story.GetStories).Methods("GET")           // GET method to list/get all stories
+	router.HandleFunc("/stories/{id:[0-9]+}", story.GetStory).Methods("GET") // GET to get specific story
 	fmt.Println("Serving on :9000")
 	log.Fatal(http.ListenAndServe(":9000", router))
 }
